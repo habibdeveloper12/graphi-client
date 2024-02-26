05494ed9be7b8d7d2290973e7f2351df3d04f12a
habibur rahman <habibur.rahman@elementzerolabs.com>
2024-02-26 11:04:40 +0600

    df

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,18 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_krtBxeg-no1hoBH33tztcR65u1QSMQ4",
-  authDomain: "graphi-b5822.firebaseapp.com",
-  projectId: "graphi-b5822",
-  storageBucket: "graphi-b5822.appspot.com",
-  messagingSenderId: "157898007781",
-  appId: "1:157898007781:web:a487ddc7c74d12a607600c"
-};
+  apiKey: "AIzaSyCsz9LWO7XrkcfDtDjmH8kBf9fdF9gusPA",
 
+  authDomain: "graphi-ba9fb.firebaseapp.com",
+
+  projectId: "graphi-ba9fb",
+
+  storageBucket: "graphi-ba9fb.appspot.com",
+
+  messagingSenderId: "937813803125",
+
+  appId: "1:937813803125:web:9c1d8534c8a6616390b83c",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
